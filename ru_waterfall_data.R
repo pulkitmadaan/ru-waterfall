7944acfe53245d0048dfeb146f3ad681a7696098
pulkit madaan <pulkit.madaan@flipkart.com>
2018-01-03 13:10:33 +0530

    serviceability update

--- ru_waterfall_data.R
+++ ru_waterfall_data.R
@@ -21,7 +21,7 @@ temp_week<-week(wk_end_date)
 
 #### Reading Files ####
 # list.files(ru_path)
-sales<- read.csv(paste0(ru_path,"/fsn_fc_pincode_day_sales.csv"),nrows=2000)
+sales_raw<- read.csv(paste0(ru_path,"/fsn_fc_pincode_day_sales.csv"),nrows=2000)
 lzn_mapping <- read.csv(paste0(ru_path,"/lzn_mapping.csv"))
 vendor_site <- read.csv(paste0(ru_path,"/vendor_site.csv"))
 lead_time <- read.csv(paste0(ru_path,"/fsn_fc_lead_time.csv"),nrows=2000)
@@ -35,8 +35,11 @@ explain_ff <- read.csv(paste0(ru_path,"/explain_ff.csv"),nrows=2000)
 vendor_adherence <- read.csv(paste0(ru_path,"/fsn_fc_vendor_adherence.csv"),nrows=2000)
 
 # Basic Data Cleaning 
-sales <- sales %>% filter(fsn!="") %>% mutate(nat_sales = sales-ru_sales) %>%
-  rename(dest_pincode = destination_pincode)
+sales <- sales_raw %>% filter(fsn!="") %>% 
+  rename(dest_pincode = destination_pincode) %>%
+  mutate(ru_loss = sales-ru_sales, 
+         dest_pincode = as.factor(dest_pincode))
+  
 
 lzn_mapping <- lzn_mapping %>% filter(lzn_value!="" & lzn_value!='0') %>%
   rename(fc=src_fc_code) %>% 
@@ -44,11 +47,13 @@ lzn_mapping <- lzn_mapping %>% filter(lzn_value!="" & lzn_value!='0') %>%
   
 lzn_mapping_2 <- subset(lzn_mapping, dest_pincode %in% sales$dest_pincode)
 
+# Creating combination of dest_pincode, fsn, fc from 
 dest_fsn <- sales %>% select(fsn,dest_pincode,brand,bu,category,vertical) %>% distinct %>%
   mutate(dest_pincode=as.factor(dest_pincode))
-fc<-data.frame(unique(lzn_mapping_2$fc))
-dest_fsn_fc <- merge(dest_fsn,fc,by=NULL) 
+fc<-data.frame(unique(lzn_mapping_2$fc)) # FCs used for fulfilling to sales dest pincodes
+dest_fsn_fc <- merge(dest_fsn,fc,by=NULL)  
 names(dest_fsn_fc)[ncol(dest_fsn_fc)]<- "fc"
 
-vendor_site_2 <- vendor_site %>% group_by(fsn,fc) %>% summarize(vendors=n_distinct(vsid)) %>% as.data.frame()
+
+vendor_site_2 <- vendor_site %>% group_by(fsn,fc) %>% summarize(vendor_count=n_distinct(vsid)) %>% as.data.frame()
 
--- ru_waterfall_script.R
+++ ru_waterfall_script.R
@@ -1,9 +1,22 @@
 #### Placement Issues - Placement NOT Possible
-# Creating Master files
+# Creating Master pincode files
 master_temp <- left_join(dest_fsn_fc,lzn_mapping,by=c("dest_pincode","fc"))
 master_temp_1 <- subset(master_temp, !(is.na(lzn_value))) 
 nrow(master_temp) == nrow(master_temp_2) # Check if nas are passed in lzn_mapping despite removing thru filters
 master_temp_2<- left_join(master_temp_1,vendor_site_2,by=c("fsn","fc")) %>% 
-  mutate(vendor_avl_flag=ifelse(is.na(vendors),0,1)) # Creating vendor flag
+  mutate(vendor_avl_flag=ifelse(is.na(vendor_count),0,1)) # Creating vendor flag
 
-#creation of vendor flag
+# Creating Serviceability flag - to separate Large FCs as source
+distinct_dp <- lzn_mapping %>% select(dest_pincode) %>% distinct 
+serviceability <- lzn_mapping %>% filter(lzn_value %in% c('L1','L2','Z1','Z2')) %>%
+  group_by(dest_pincode) %>% summarise(lz_fc_count = n_distinct(fc)) %>% 
+  right_join(distinct_dp, by='dest_pincode') %>%
+  mutate(lz_fc_count = ifelse(is.na(lz_fc_count),0,lz_fc_count),
+         is_serviceable_flag = ifelse(lz_fc_count==0,0,1))
+
+# L
+sales_fsn_dp <- sales %>% group_by(dest_pincode,fsn) %>%
+  summarise(sales=sum(sales),
+            ru_sales=sum(ru_sales),
+            ru_loss=sum(ru_loss),
+            rru_sales=sum(rru_sales))
