6bc341d8d25aa70a246a9cf6dfecfb6e180ed2d7
pulkit madaan <pulkit.madaan@flipkart.com>
2018-01-17 00:03:34 +0530

    Data 17 Jan

--- ru_waterfall_data.R
+++ ru_waterfall_data.R
@@ -17,32 +17,32 @@ wk_start_date <-wk_end_date-6
 leadtime_start_date<-wk_start_date-70 # 10 weeks
 low_sales_end_date <- as.numeric(format(wk_end_date, format="%Y%m%d")) 
 low_sales_start_date <- as.numeric(format(wk_end_date-28, format="%Y%m%d"))
-temp_week<-week(wk_end_date)
+ru_week<-week(wk_end_date)
 
 
 #### Reading Files ####
-list.files(ru_path)
+# list.files(ru_path)
 sales_raw <- read.csv(paste0(ru_path,"/fsn_fc_pincode_day_sales.csv")
-                     ,nrows = 10000
+                     # ,nrows = 1000000
                      )
 lzn_mapping <- read.csv(paste0(ru_path,"/lzn_mapping.csv"))
 vendor_site <- read.csv(paste0(ru_path,"/vendor_site.csv"))
 lead_time <- read.csv(paste0(ru_path,"/fsn_fc_lead_time.csv"))
 cat_lead_time <- read.csv(paste0(ru_path,"/cat_lead_time.csv"))
-# min_sale_threshold <- read.csv(paste0(ru_path,"/min_sales_Depth.csv"))
+min_sale_threshold <- read.csv(paste0(ru_path,"/min_sales_Depth.csv"))
 exclusion_list <- read.csv(paste0(ru_path,"/exclusion_list.csv"))
 preferred_warehouse <- read.table(paste0(ru_path,"/preferred_warehouse.tsv"),header = T)
 # fsn_placement <- read.csv(paste0(ru_path,"/fsn_placement.csv"),nrows=2000)
 fsn_fc_l0 <- read.csv(paste0(ru_path,"/fsn_fc_l0.csv"),nrows=100000) # nulls till 50L
 explain_ff <- read.csv(paste0(ru_path,"/explain_ff.csv"),nrows = 5000000)
-vendor_adherence <- read.csv(paste0(ru_path,"/fsn_fc_vendor_adherence.csv"),nrows=100000)
-promotions <- read.csv(paste0(ru_path,"/fsn_pincode_promotion_sales.csv"),nrows=10000)
+vendor_adherence <- read.csv(paste0(ru_path,"/fsn_fc_vendor_adherence.csv"))
+promotions <- read.csv(paste0(ru_path,"/fsn_pincode_promotion_sales.csv"))
 
 #### Basic Data Cleaning ####
-sales_raw_2 <- sales_raw %>% filter(week==49)
+sales_raw_2 <- sales_raw %>% filter(week==ru_week)
 sales <- sales_raw_2 %>% filter(fsn!="") %>%
   rename(dest_pincode = destination_pincode) %>%
-  mutate(ru_loss = sales-ru_sales,
+  mutate(nat_sales = sales-ru_sales,
          dest_pincode = as.factor(dest_pincode))
 
 lzn <- lzn_mapping %>% filter(lzn_value!="" & lzn_value!='0' & lzn_value!='NULL') %>%
@@ -53,13 +53,14 @@ lz <- lzn %>% filter(lzn_value %in% c("L1","L2","Z1","Z2"))
 
 vendor_site_2 <- vendor_site %>% group_by(fsn,fc) %>% summarize(vendor_count=n_distinct(vsid)) %>% as.data.frame()
 
-# explain_ff <- explain_ff %>% rename(dest_pincode = destination_pincode)
-vendor_adherence %<>% mutate(poi_created_day = as.Date(as.character(poi_created_day), format = "%Y%m%d"),
-        category="Book") # to be removed
+fsn_category <- sales[!duplicated(sales[,c(3,7)]),c(3,7)] # Fsn - category list for week's sales
+
+vendor_adherence %<>% mutate(poi_created_day = as.Date(as.character(poi_created_day), format = "%Y%m%d")) %>%
+  right_join(fsn_category, by="fsn") # join to be removed, right join to only capture 
 
 cat_lead_time %<>% rename(cat_lead_time = lead_time)
 lead_time %<>% 
-  mutate(category="Book") %>% # to be removed 
+  right_join(fsn_category, by="fsn") %>% # join to be removed 
   rename(fc=warehouse) %>%
   left_join(cat_lead_time, by = 'category') %>%
   mutate(policy_lt=ifelse(lead_time<1,cat_lead_time,lead_time),
@@ -67,10 +68,14 @@ lead_time %<>%
   select(-bu,-super_cat) 
 
 
-preferred_fc <- preferred_warehouse %>% mutate(pref_fc=substr(preferred_fc_non_large,1,regexpr(',',preferred_fc_non_large)-1),
-                                               pincode=as.factor(as.character(pincode))) %>%
-  select(pincode, pref_fc) %>% rename(dest_pincode=pincode)
+preferred_warehouse %<>% mutate(pincode=as.factor(as.character(pincode))) %>%
+  select(pincode, preferred_fc_non_large) %>% rename(dest_pincode=pincode, preferred_fc=preferred_fc_non_large)
+
+preferred_fc <- preferred_warehouse %>% mutate(pref_fc=substr(preferred_fc,1,regexpr(',',preferred_fc)-1)) %>%
+  select(dest_pincode, pref_fc)
 
 names(promotions) <- c("fsn", "dest_pincode", "day", "week_num", "month", "year", "offer_sales") # to be removed after fact promoted to production
-promotions_fsn_dp <- promotions %>% filter(week_num==temp_week) %>% group_by(fsn, dest_pincode) %>%
+promotions_fsn_dp <- promotions %>% filter(week_num==ru_week) %>% group_by(fsn, dest_pincode) %>%
   summarise(offer_sales = sum(offer_sales))
+
+source("fsn_fc_l0_aggregation.R")
